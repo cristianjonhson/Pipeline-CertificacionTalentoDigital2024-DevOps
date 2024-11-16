64e4fa04e7b2a634a3782fcdc824ba0b39fcc2a3
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-11-16 10:11:44 -0300

    feat: Creacion de la estructura base del proyecto

---
+++ src/main/java/cl/talentodigital/Certificaciondevops2024/Certificaciondevops2024Application.java
@@ -0,0 +1,13 @@
+package cl.talentodigital.Certificaciondevops2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Certificaciondevops2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Certificaciondevops2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/talentodigital/Certificaciondevops2024/Certificaciondevops2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.talentodigital.Certificaciondevops2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Certificaciondevops2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
