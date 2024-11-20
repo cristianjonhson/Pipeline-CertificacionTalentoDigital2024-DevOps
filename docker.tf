c261a33c4e9952f93659b477e72af76fe41dacd7
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-11-20 01:22:50 -0300

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -13,23 +13,24 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"  # Dirección del socket de Docker en el sistema local
 }
 
-# Recurso de la imagen Docker: Define cómo se debe construir y etiquetar la imagen de Docker
+# Recurso de la imagen Docker personalizada: Construir la imagen desde el Dockerfile
 resource "docker_image" "my_app_image" {
   name     = var.image_name  # Usamos la variable 'image_name' para nombrar la imagen Docker
-  platform = "linux/amd64"   # Especifica que la plataforma debe ser linux_amd64
+  platform = "linux/amd64"   # Especifica que la plataforma debe ser linux/amd64
 
-  # Bloque de construcción de la imagen Docker
+  # Bloque de construcción de la imagen Docker usando el Dockerfile
   build {
     context = "."  # El contexto es el directorio actual (donde se encuentra el Dockerfile)
+    dockerfile = "./Dockerfile"  # Especificamos la ubicación del Dockerfile
   }
 }
 
-# Recurso del contenedor Docker: Define el contenedor que se va a ejecutar
+# Recurso del contenedor Docker: Define el contenedor que se va a ejecutar a partir de la imagen creada
 resource "docker_container" "my_app_container" {
   image = docker_image.my_app_image.latest  # Usamos la última versión de la imagen creada previamente
   name  = var.image_name  # Usamos la variable 'image_name' para el nombre del contenedor
 
-  # Configuración de puertos: Exponemos el puerto 8080 del contenedor hacia el host
+  # Configuración de puertos: Exponemos el puerto 8082 del contenedor hacia el host
   ports {
     internal = var.docker_port   # Usamos la variable 'docker_port' para el puerto interno
     external = var.docker_port   # Usamos la misma variable para el puerto externo
