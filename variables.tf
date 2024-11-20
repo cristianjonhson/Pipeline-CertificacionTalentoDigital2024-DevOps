e10273bcc98d006fe7f18630101d088cc35602ba
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-11-19 23:54:28 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,15 @@
 # variables.tf
 
+# Definir una variable para el nombre de la imagen Docker (puede ser útil si deseas personalizarla)
 variable "image_name" {
   description = "El nombre de la imagen Docker"
   type        = string
-  default     = "myapp"
+  default     = "myapp"  # Valor predeterminado
 }
 
+# Definir una variable para el puerto
 variable "docker_port" {
   description = "Puerto que se expondrá en el contenedor"
   type        = number
-  default     = 8080
+  default     = 8080  # Puerto predeterminado
 }
