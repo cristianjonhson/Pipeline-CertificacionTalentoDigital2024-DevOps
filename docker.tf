5bebbe6a4c2022baec0f2026299c960fce45b505
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-11-20 23:37:41 -0300

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -17,18 +17,18 @@ provider "docker" {
 resource "docker_image" "my_app_image" {
   name     = var.image_name  # Usamos la variable 'image_name' para nombrar la imagen Docker
   platform = "linux/amd64"   # Especifica que la plataforma debe ser linux/amd64
-
-  # Bloque de construcción de la imagen Docker usando el Dockerfile
-  build {
-    context = "."  # El contexto es el directorio actual (donde se encuentra el Dockerfile)
-    dockerfile = "./Dockerfile"  # Especificamos la ubicación del Dockerfile
-  }
 }
 
 # Recurso del contenedor Docker: Define el contenedor que se va a ejecutar a partir de la imagen creada
 resource "docker_container" "my_app_container" {
   image = docker_image.my_app_image.name  # Usamos la última versión de la imagen creada previamente
   name  = var.image_name  # Usamos la variable 'image_name' para el nombre del contenedor
+
+# Bloque de construcción de la imagen Docker usando el Dockerfile
+  build {
+    context = "."  # El contexto es el directorio actual (donde se encuentra el Dockerfile)
+    dockerfile = "./Dockerfile"  # Especificamos la ubicación del Dockerfile
+  }
 
   # Configuración de puertos: Exponemos el puerto 8082 del contenedor hacia el host
   ports {
